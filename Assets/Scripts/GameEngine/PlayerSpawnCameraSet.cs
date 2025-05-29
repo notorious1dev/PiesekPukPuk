b34cd78201f3f2f21e23a648da498e32be2f53c4
defektonec <sarjovskiy.dima@gmail.com>
2025-05-29 19:09:42 +0200

    camera follow

---
+++ Assets/Scripts/GameEngine/PlayerSpawnCameraSet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Fusion;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+
+public class PlayerSpawnCameraSet : NetworkBehaviour
+{
+    private CinemachineCamera _camera;
+    public override void Spawned()
+    {
+        if (!Object.HasInputAuthority) return;
+        _camera = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineCamera>();
+
+        if (_camera == null )
+        {
+            Debug.LogWarning("Camera not found");
+            return;
+        }
+            _camera.LookAt = transform;
+            _camera.Follow = transform;
+    }
+}
